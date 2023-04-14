310c1da4c5dc9656f52b2e0b6ca3c42dd10cb9c2
priyankasingh <psingh.c@stc.com.sa>
2023-04-14 21:25:25 +0530

    Added a static SPA component - Header

---
+++ ui.frontend/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+//Header.js
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+
+    render() {
+        return (
+                <header className="Header">
+                    <div className="Header-container">
+                        <h1>Knowledge-Meet</h1>
+                    </div>
+                </header>
+        );
+    }
+}
